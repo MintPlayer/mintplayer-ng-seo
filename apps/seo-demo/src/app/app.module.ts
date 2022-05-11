f6bd4d231426f8f1d8cbf36c7da333800f074051
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2022-05-11 15:07:25 +0200

    Removed NxWelcomeComponent refs

--- apps/seo-demo/src/app/app.module.ts
+++ apps/seo-demo/src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
