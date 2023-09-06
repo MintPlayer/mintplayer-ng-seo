0c41c3a5046b3e8784a7e9fc54b0f190fa77964a
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-09-06 15:37:31 +0200

    Split hreflang

--- libs/mintplayer-ng-seo/src/lib/directives/href-lang/href-lang.directive.ts
+++ libs/mintplayer-ng-seo/src/lib/directives/href-lang/href-lang.directive.ts
@@ -1,7 +1,7 @@
 import { APP_BASE_HREF, DOCUMENT } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Directive, Inject, Input, OnDestroy, Optional, Renderer2 } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
+import { NavigationExtras, Params, Router } from '@angular/router';
 import { ROUTER, IRouter } from '@mintplayer/ng-router-provider';
 import { BehaviorSubject, Observable, Subject, combineLatest, map, takeUntil } from 'rxjs';
 import { CommandsAndExtras } from '../../interfaces/commands-and-extras';
@@ -20,16 +20,10 @@ export class HrefLangDirective implements OnDestroy {
     this.document = <Document>document;
     this.router = advancedRouter || router;
 
-    this.hreflang$ = combineLatest([this.language$, this.commands$, this.extras$])
-      .pipe(map(([language, commands, extras]) => {
-        if (language) {
-          return { [language]: <CommandsAndExtras>{ commands, extras } };
-        } else {
-          return null;
-        }
+    this.extras$ = combineLatest([this.queryParams$, this.fragment$])
+      .pipe(map(([queryParams, fragment]) => {
+        return <NavigationExtras>{ queryParams, fragment };
       }));
-      
-
 
     combineLatest([this.language$, this.commands$, this.extras$])
       .pipe(takeUntil(this.destroyed$))
@@ -57,24 +51,24 @@ export class HrefLangDirective implements OnDestroy {
   private router: Router | IRouter;
   private tags: HTMLLinkElement[] = [];
 
-  // private hreflang$ = new BehaviorSubject<Record<string, CommandsAndExtras>>({});
   private language$ = new BehaviorSubject<string | null>(null);
   private commands$ = new BehaviorSubject<any[]>([]);
-  private extras$ = new BehaviorSubject<NavigationExtras | null>(null);
-  private hreflang$: Observable<Record<string, CommandsAndExtras> | null>;
+  private queryParams$ = new BehaviorSubject<Params | null>(null);
+  private fragment$ = new BehaviorSubject<string | null>(null);
+  private extras$: Observable<NavigationExtras | null>;
   private destroyed$ = new Subject();
 
-  // @Input() set hrefLang(value: Record<string, CommandsAndExtras>) {
-  //   this.hreflang$.next(value);
-  // }
   @Input() set hrefLang(value: string) {
     this.language$.next(value);
   }
   @Input() set commands(value: any[]) {
     this.commands$.next(value);
   }
-  @Input() set extras(value: NavigationExtras) {
-    this.extras$.next(value);
+  @Input() set queryParams(value: Params | undefined | null) {
+    this.queryParams$.next(value ?? null);
+  }
+  @Input() set fragment(value: string | undefined | null) {
+    this.fragment$.next(value ?? null);
   }
 
   ngOnDestroy() {
